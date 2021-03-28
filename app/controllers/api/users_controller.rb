e929e22dc2cc540d93bfbb9e2821070ae3ada96a
katrinajelinek <kmrjelinek@gmail.com>
2021-03-28 13:31:35 -0600

    adds users create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      username: params[:username],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      image_url: params[:image_url],
+      disclaimer_agreement: params[:disclaimer_agreement],
+      terms_and_conditions_agreement: params[:terms_and_conditions_agreement],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
