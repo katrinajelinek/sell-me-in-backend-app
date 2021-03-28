786860519f5e39e557309553df53444f8702388f
katrinajelinek <kmrjelinek@gmail.com>
2021-03-28 17:22:22 -0600

    changes authentication of messages

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,5 @@
 class Api::MessagesController < ApplicationController
-  before_action :authenticate_user, except: [:show]
+  before_action :authenticate_user
 
   def show
     @message = Message.find(params[:id])
