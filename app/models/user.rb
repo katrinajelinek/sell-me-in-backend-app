2a3c89b7c67fdf9ab9d6712390ea4544ea720e0e
katrinajelinek <kmrjelinek@gmail.com>
2021-03-28 17:40:39 -0600

    adds user validations for first_name, last_name, and acceptance for terms_and_conditions_agreement, and disclaimer_agreement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :disclaimer_agreement, acceptance: true
+  validates :terms_and_conditions_agreement, acceptance: true
 
   has_many :messages
   has_many :posts
