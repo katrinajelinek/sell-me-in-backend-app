b37097e9b954f6b10006b84db76b325db43fd445
katrinajelinek <kmrjelinek@gmail.com>
2021-03-28 17:49:09 -0600

    adds validations for title, video_url, description, location, and price in posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   has_many :categories, through: :post_categories
   has_many :images
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :video_url, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :description, length: { maximum: 500 }
 end
