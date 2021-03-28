eeb5e65e7ec94ec38182ef89acd4cc2558be5ab0
katrinajelinek <kmrjelinek@gmail.com>
2021-03-28 15:58:09 -0600

    adds post create logic

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,4 +1,6 @@
 class Api::PostsController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+
   def index
     @posts = Post.all
     render "index.json.jb"
@@ -7,5 +9,21 @@ class Api::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     render "show.json.jb"
+  end
+
+  def create
+    @post = Post.new(
+      user_id: current_user.id,
+      title: params[:title],
+      description: params[:description],
+      video_url: params[:video_url],
+      price: params[:price],
+      location: params[:location],
+    )
+    if @post.save
+      render "show.json.jb"
+    else
+      render json: { errors: @post.errors.full_messages }, status: :bad_request
+    end
   end
 end
