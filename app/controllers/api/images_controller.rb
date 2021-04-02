a6b073200a3c09138cd3555eb7e2a1d5358cf0c0
katrinajelinek <kmrjelinek@gmail.com>
2021-04-02 15:37:20 -0600

    adds cloudinary logic to image create

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -2,9 +2,13 @@ class Api::ImagesController < ApplicationController
   before_action :authenticate_user
 
   def create
+    if params[:image]
+      response = Cloudinary::Uploader.upload(params[:image], resource_type: :auto)
+      cloudinary_url = response["secure_url"]
+    end
     @image = Image.new(
       post_id: params[:post_id],
-      image_url: params[:image_url],
+      image_url: cloudinary_url,
     )
     if @image.save
       render "show.json.jb"
