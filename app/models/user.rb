164cdee6133f19d2035a3470976e0a682c627a45
katrinajelinek <kmrjelinek@gmail.com>
2021-05-24 23:08:12 -0600

    adds diclaimer and terms and conditions truthy check

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
 
   has_many :messages, dependent: :destroy
   has_many :posts, dependent: :destroy
+
+  def disclaimer_agreement_value
+    if disclaimer_agreement != true
+      errors.add(:disclaimer_agreement, "Disclaimer agreement must be selected")
+    end
+  end
+
+  def terms_and_conditions_agreement_value
+    if terms_and_conditions_agreement != true
+      errors.add(:terms_and_conditions_agreement, "Disclaimer agreement must be selected")
+    end
+  end
 end
